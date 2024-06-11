224047d84fa84e9a49f4616bbee16c48c85c8877
Felipe Rios <felipe.rios.silva@outlook.com>
2024-06-11 16:35:40 -0400

    fix(auto-init): fixed the auto-init when it's running the "workspace" command
    - corrected the "clear" command to remove all folders instead of just the first
    
    Signed-off-by: Felipe Rios <felipe.rios.silva@outlook.com>

--- cmd/terra/rootCommand.go
+++ cmd/terra/rootCommand.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/hashicorp/hcl/v2/hclsimple"
 	"github.com/joho/godotenv"
 	logger "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 // Main command logic
@@ -23,15 +25,51 @@ var rootCmd = &cobra.Command{
 			logger.Warnf("Error loading .env file: %s", err)
 		}
 		format()
-		// if the command to be executed is "init" don't change the workspace
-		if terraArgs[0] != "init" {
-			changeWorkspace(absDir) // TODO: create a flag to disable this
-		}
+
+		//if !isInitNeeded(absDir) {
+		_ = runInDir("terragrunt", []string{"init"}, absDir)
+		//}
+		// TODO: this is not working with tfvars file
+		changeWorkspace(absDir)
+
 		err = runInDir("terragrunt", terraArgs, absDir)
 		if err != nil {
 			logger.Fatalf("Terragrunt command failed: %s", err)
 		}
 	},
+}
+
+type Terraform struct {
+	Source string `hcl:"source,optional"`
+}
+
+type Terragrunt struct {
+	Terraform Terraform `hcl:"terraform,block"`
+}
+
+func isInitNeeded(absDir string) bool {
+	// Construct the path to the terragrunt.hcl file
+	hclPath := filepath.Join(absDir, "terragrunt.hcl")
+
+	// Parse the terragrunt.hcl file
+	var tgConfig Terragrunt
+	hclsimple.DecodeFile(hclPath, nil, &tgConfig)
+
+	// Extract the "needToBeFound" path from the "source" field
+	needToBeFound := strings.TrimPrefix(tgConfig.Terraform.Source, "../..//")
+
+	// Construct the path to the ".terraform/providers" directory
+	providerDir := filepath.Join(absDir, ".terragrunt-cache", "*", "*", needToBeFound, ".terraform", "providers")
+
+	// Check if the ".terraform/providers" directory exists
+	_, err := os.Stat(providerDir)
+	if os.IsNotExist(err) {
+		// The ".terraform/providers" directory does not exist, so "init" is needed
+		return true
+	}
+
+	// The ".terraform/providers" directory exists, so "init" is not needed
+	return false
 }
 
 func changeWorkspace(dir string) {
