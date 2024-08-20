2cbda097858aa3055ed54357e6449f0d0004ecd4
Felipe Rios <felipe.rios.silva@outlook.com>
2024-08-20 19:23:54 -0400

    feat(validation): created validation on the Settings entity
    Signed-off-by: Felipe Rios <felipe.rios.silva@outlook.com>

--- internal/domain/entities/settings.go
+++ internal/domain/entities/settings.go
@@ -1,12 +1,13 @@
 package entities
 
 import (
+	"github.com/go-playground/validator/v10"
 	"github.com/kelseyhightower/envconfig"
 	logger "github.com/sirupsen/logrus"
 )
 
 type Settings struct {
-	TerraCloud               string `envconfig:"TERRA_CLOUD"                 required:"false"`
+	TerraCloud               string `envconfig:"TERRA_CLOUD"                 required:"false" validate:"oneof=aws azure"`
 	TerraTerraformWorkspace  string `envconfig:"TERRA_WORKSPACE"             required:"false"`
 	TerraAwsRoleArn          string `envconfig:"TERRA_AWS_ROLE_ARN"          required:"false"`
 	TerraAzureSubscriptionID string `envconfig:"TERRA_AZURE_SUBSCRIPTION_ID" required:"false"`
@@ -18,5 +19,12 @@ func NewSettings() *Settings {
 	if err != nil {
 		logger.Fatalf("Failed to process environment variables: %s", err)
 	}
+
+	validate := validator.New()
+	err = validate.Struct(settings)
+	if err != nil {
+		logger.Fatalf("Environment variables validation error: %v", err)
+	}
+
 	return &settings
 }
